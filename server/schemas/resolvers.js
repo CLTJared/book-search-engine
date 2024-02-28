12af0ada6116594fe7c15a062d39d966aa257dc7
Jared Elliott <bootcamp@jaredclt.com>
2024-02-27 21:44:31 -0500

    Updated typeDefs and created resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,52 @@
+const { signToken, AuthenticationError } = require('../utils/auth');
 const { Book, User } = require('../models');
 
 const resolvers = {
+    Query: { // Lesson 26 - Resolver Context | authentication/find user
+        me: async (parent, args, context) => {
+            if (context.user) return User.findOne({ _id: context.user._id })
+            throw AuthenticationError;
+        },
+    },
+
+    Mutation: { // Lesson 26 - Resolver Context
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+                if (!user) throw AuthenticationError;
+
+            const correctPw = await user.isCorrectPassword(password);
+                if (!correctPw) throw AuthenticationError;
+    
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        addBook: async (parent, { newBook }, context) => {
+            if (context.user) { 
+            return User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $push: { savedBooks: newBook }},
+                { new: true });
+            } 
+            throw AuthenticationError;
+        },
+
+        removeBook: async (parent, { bookID }, context) => {
+            if (context.user) {
+            return await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookID }}},
+                { new: true }
+            );
+            }
+            throw AuthenticationError;
+        }}
 
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -15,15 +15,34 @@ type User {
     savedBooks: [Book]
 }
 
+type Auth {
+    token: ID!
+    user: User
+}
+
 type newUser {
     username: String!
     email: String!
     password: String!
 }
 
+input newBook {
+    _id: ID!
+    authors: String
+    description: String!
+    bookId: Int!
+    image: String
+    title: String!
+  }
+
+type Query {
+    me: User
+}
+
 type Mutation {
+    login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): newUser
-    saveBook(newBook: InputBook!): User
+    addBook(newBook: newBook!): User
     removeBook(bookId: ID!): User
 }
 `;
